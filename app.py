ab5d51805d5b1693d4e439f468b168f5ae834dd1
KirsanowArtem <kirsanov.artem.d@gmail.com>
2024-12-08 22:17:30 +0200

    08.12.2024/22:17

--- app.py
+++ app.py
@@ -101,8 +101,9 @@ if __name__ == '__main__':
 
 
 """
-if __name__ == 'main':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
